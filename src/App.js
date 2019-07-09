17efea67f89215cf079ce5cd9b824f58038a7f18
Aleksandr <duxard@gmail.com>
2019-07-09 04:33:32 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <h1>Start</h1>
       </header>
     </div>
